874c82c5ab94cc82e25f1f27a72526deb2865e94
RMarutyan <ruzanna_marutyan@epam.com>
2022-11-15 01:30:16 +0400

    first commit'

---
+++ Lesson_1/src/Lesson1.java
@@ -0,0 +1,30 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.print("Hi\n");
+        System.out.println("Lesson1");
+        byte x=127;
+        System.out.println(x);
+        short y =1300;
+        System.out.println(y);
+        int a=30000;
+        System.out.println(a);
+        long b =242741724524L;
+        System.out.println(b);
+        float fl= 22.1f;
+        System.out.println(fl);
+        double db= 159.7;
+        db=245.8;
+        System.out.println(db);
+        boolean bl= false;
+        System.out.println(bl);
+        bl=true;
+        System.out.println(bl);
+        char ch='f';
+        System.out.println(ch);
+        char un= '\u0025';
+        un= '\u2764';
+        System.out.println(un);
+
+
+            }
+}
